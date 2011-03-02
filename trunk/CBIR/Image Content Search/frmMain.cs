319c287ddb0037195dbebc918741f38dd712768f
nguyentheloc@gmail.com <nguyentheloc@gmail.com@9fa8102b-ed27-315b-f898-4a89050693df>
2011-03-02 04:27:45 +0000

    Image DB & Query --> Have lunch!

--- trunk/CBIR/Image Content Search/frmMain.cs
+++ trunk/CBIR/Image Content Search/frmMain.cs
@@ -21,40 +21,47 @@ namespace Image_Content_Search
         {
             if (ofdBrowseImage.ShowDialog(this) == DialogResult.OK)
             {
-                //Hien thi anh len pbImageTarget
-                Bitmap bmTarget = (Bitmap)Bitmap.FromFile(ofdBrowseImage.FileName);
+                //Hien thi anh len pbQueryImage
+                Bitmap bmQuery = (Bitmap)Bitmap.FromFile(ofdBrowseImage.FileName);
 
                 //Test 1 so Function trong Lib
-                //ZinImageLib.ToBinaryImage(bmTarget, 100);
-                //ZinImageLib.ToGrayScale(bmTarget);
-                //bmTarget = ZinImageLib.ToGrayScale2(bmTarget);
-                //bmTarget = ZinImageLib.RotateImage(bmTarget, 45f);
+                //ZinImageLib.ToBinaryImage(bmQuery, 100);
+                //ZinImageLib.ToGrayScale(bmQuery);
+                //bmQuery = ZinImageLib.ToGrayScale2(bmQuery);
+                //bmQuery = ZinImageLib.RotateImage(bmQuery, 45f);
              
-                pbImageTarget.Image = bmTarget;
+                pbQueryImage.Image = bmQuery;
 
-                //MessageBox.Show(bmTarget.VerticalResolution.ToString() + "--" + bmTarget.HorizontalResolution.ToString());
+                //MessageBox.Show(bmQuery.VerticalResolution.ToString() + "--" + bmQuery.HorizontalResolution.ToString());
 
                 //Vẽ lưới 10 * 10 lên ảnh xem nào
-                int iCellWidth = bmTarget.Width / 10;
-                int iCellHeight = bmTarget.Height / 10;
-                for (int i = 0; i < bmTarget.Width; i++)
+                int iCellWidth = bmQuery.Width / ZinImageLib.GridCols;
+                int iCellHeight = bmQuery.Height / ZinImageLib.GridRows;
+                for (int i = 0; i < bmQuery.Width; i++)
                     if (i % iCellWidth == 0)
                     {
-                        for (int j = 0; j < bmTarget.Height; j++)
+                        for (int j = 0; j < bmQuery.Height; j++)
                         {
-                            bmTarget.SetPixel(i, j, Color.FromArgb(255, 0, 0));
+                            bmQuery.SetPixel(i, j, Color.FromArgb(255, 0, 0));
                         }
                     }
 
-                for (int j = 0; j < bmTarget.Height; j++)
+                for (int j = 0; j < bmQuery.Height; j++)
                     if (j % iCellHeight == 0)
-                        for (int i = 0; i < bmTarget.Width; i++)
-                            bmTarget.SetPixel(i, j, Color.Red);
+                        for (int i = 0; i < bmQuery.Width; i++)
+                            bmQuery.SetPixel(i, j, Color.Red);
             }
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
+            //Tách các Rectangle chứa đối tượng (hình dạng) ra khỏi ảnh -> lưu vào 1 mảng (rectangle)        
+            //Duyệt qua mảng -> xử lý với từng đối tượng cụ thể
+            // - Với mỗi đối tượng cần tìm -> tìm 4 lần ứng với 4 trường hợp (xoay)
+            // - Tìm trục chính, trục phụ, tâm sai
+            // - Chuẩn hóa xoay
+            // - Resize ảnh về kích thước sao cho trục chính luôn cố định = 192px
+            // - Phủ lưới lên đối tượng này -> trích chọn -> dãy nhị phân
 
         }
     }
